c3b35fef52c6667f0679fc05408e29e9a7596b58
Onduso <londuso@us.ci.org>
2021-06-24 14:30:02 +0300

    Added ssl

--- application/models/Email_model.php
+++ application/models/Email_model.php
@@ -51,7 +51,7 @@ class Email_model extends CI_Model {
         $config['mailpath']		= "/usr/bin/sendmail"; // or "/usr/sbin/sendmail"
         $config['protocol']		= "smtp";
 		//$config['smtp_auth'] = TRUE;
-        $config['smtp_host']	= "ssl://smtp.office365.com";
+        $config['smtp_host']	= "smtp.office365.com";
         $config['smtp_port']	= "587";
 		$config['smtp_crypto'] = 'tls';
 		$config['smtp_user']='afrstaffrecognition@ke.ci.org';
