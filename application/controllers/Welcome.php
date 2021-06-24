619ed3ca127a0ddb0e7f97c6bdf1ac2cc8ada32b
Onduso <londuso@us.ci.org>
2021-06-24 12:51:41 +0300

    Changed email to gmail one"

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -22,7 +22,7 @@ class Welcome extends CI_Controller {
 	{
 		$this->load->model('Email_model');
 
-		$this->Email_model->account_opening_email('staff','nkarisa@ke.ci.org');
+		$this->Email_model->account_opening_email('staff','nkmwambs@gmail.com');
 		$this->load->view('welcome_message');
 
 	}
