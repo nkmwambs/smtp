18f9b9b4629ce957a2ebb5b13a95403b243b9813
Onduso <londuso@us.ci.org>
2021-06-28 11:19:24 +0300

    Updated the from email

--- application/models/Email_model.php
+++ application/models/Email_model.php
@@ -54,10 +54,10 @@ class Email_model extends CI_Model {
 		$config['smtp_auth'] = TRUE;
       //  $config['smtp_host']	= "smtp.office365.com";
 
-		$config['smtp_host']	= "smtp.gmail.com";
+		$config['smtp_host']	= "‘ssl://smtp.gmail.com’";
 
-        $config['smtp_port']	= "587";//
-		$config['smtp_crypto'] = 'STARTTLS';
+        $config['smtp_port']	= "465";//
+		$config['smtp_crypto'] = 'ssl';
 		// $config['smtp_user']='afrstaffrecognition@ke.ci.org';
 		// $config['smtp_pass']	= "@Compassion123";
 
